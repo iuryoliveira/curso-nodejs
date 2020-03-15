96de3964a4be6f3b91f6556257d79db719047d46
Iury <iury@rarolabs.com.br>
2020-03-15 11:44:05 -0300

    CORS

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o banco de dados
 mongoose.connect(
     'mongodb://192.168.99.100:27017/nodeapi',
     { useNewUrlParser: true,useUnifiedTopology: true }
 );
-
 requireDir('./src/models');
 
 // Rotas
