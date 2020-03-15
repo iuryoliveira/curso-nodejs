72fb022446cfa6e008e2c0fc0a15a31ab3adac9a
Iury <iury@rarolabs.com.br>
2020-03-15 00:00:36 -0300

    Criando model de produto

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o app
 const app = express();
 
 // Iniciando o banco de dados
-mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+    'mongodb://192.168.99.100:27017/nodeapi',
+    { useNewUrlParser: true,useUnifiedTopology: true }
+);
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 
 // Primeira rota
 app.get('/', (req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build Native Apps',
+        url: 'https://github.com/facebook/react-native'
+    });
+
     res.send('Hello World!');
 });
 
