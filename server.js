92ff52bab75f8bec6ef38e45379eaec190001814
Iury <iury@rarolabs.com.br>
2020-03-14 23:50:37 -0300

    Conectando a database

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o app
 const app = express();
 
+// Iniciando o banco de dados
+mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
