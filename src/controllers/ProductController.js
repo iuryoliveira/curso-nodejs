2eaa7613ce3c2b82af7f10bfe365c265c021d744
Iury <iury@rarolabs.com.br>
2020-03-15 11:17:34 -0300

    post de produto

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -7,5 +7,11 @@ module.exports = {
         const products = await Product.find();
 
         return res.json(products);
+    },
+
+    async store(req, res) {
+        const products = await Product.create(req.body);
+
+        return res.json(products);
     }
 };
